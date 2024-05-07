d3043f7da78d34a514cc00110786c8a7663b2850
jasur <itsjasur@gmail.com>
2024-05-07 14:52:53 +0900

    git ignore changes

--- lib/sensitive.dart
+++ lib/sensitive.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/foundation.dart';
 
-// String BASEURL = kReleaseMode ? "/api/" : "http://192.168.0.251:8091/api/";
+String BASEURL = kReleaseMode ? "/api/" : "http://192.168.0.251:8091/api/";
 
 // String BASEURL = "http://192.168.0.251:8091/api/"; //TEST LOCAL
 
 // String BASEURL = "https://admin.simpass.co.kr/api/"; //PROD
 
 // ignore: non_constant_identifier_names
-String BASEURL = "https://ta.simpass.co.kr/api/"; //STAGING
+// String BASEURL = "https://ta.simpass.co.kr/api/"; //STAGING
